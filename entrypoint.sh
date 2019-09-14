76deb2f009627e2ba7c90f62c281fb11e9c2d174
Shawn Toffel <shawn.toffel@gmail.com>
2019-09-14 05:29:19 +0000

    cleanup & added azure pipelines config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 # add arguments to safe_domains file
-for domain in $@; do
-  echo $domain >> /etc/squid/safe_domains
+for domain in "$@"; do
+  echo "$domain" >> /etc/squid/safe_domains
 done
 
 # start squid
